dda5f35ad61237642580db670017bfc85767c47f
ZKeiLin <zlin2016@uw.edu>
2019-04-30 09:50:24 -0700

    seperate data, finished

---
+++ app/src/main/java/edu/washington/zlin2016/quizdroid/DataModel.kt
@@ -0,0 +1,69 @@
+package edu.washington.zlin2016.quizdroid
+
+import java.io.Serializable
+
+
+val physics = Quiz(title="Physics",
+desc = "Because Physics!",
+questions = arrayOf(
+    Question(text = "What is fire?",
+        answer = 0,
+        answers= arrayOf(
+            "One of the four classical elements",
+            "A magical reaction given to us by God",
+            "A band that hasn't yet been discovered",
+            "Fire! Fire! Fire! heh-heh"
+        )
+    )))
+
+val math = Quiz(title="Math",
+desc = "Did you pass the third grade?",
+questions = arrayOf(
+    Question(text = "What is 2+2?", answer = 0, answers= arrayOf(
+        "4",
+        "22",
+        "An irrational number",
+        "Nobody knows"
+    )
+    )))
+
+val marvel = Quiz(title="Marvel Super Heroes",
+desc = "Avengers, Assemble!",
+questions = arrayOf(
+    Question(
+        text = "Who is Iron Man?",
+        answer = 0,
+        answers= arrayOf(
+            "Tony Stark",
+            "Obadiah Stane",
+            "A rock hit by Megadeth",
+            "Nobody knows"
+        )
+    ),
+    Question(
+        text = "Who founded the X-Men?",
+        answer = 1,
+        answers= arrayOf(
+            "Tony Stark",
+            "Professor X",
+            "The X-Institute",
+            "Erik Lensherr"
+        )
+    ),
+    Question(
+        text = "How did Spider-Man get his powers?",
+        answer = 0,
+        answers= arrayOf(
+            "He was bitten by a radioactive spider",
+            "He ate a radioactive spider",
+            "He is a radioactive spider",
+            "He looked at a radioactive spider"
+        )
+    )
+)
+)
+
+
+class Quiz (val title:String, val desc: String, val questions : Array<Question>) : Serializable
+
+class Question(val text: String, val answers: Array<String>, val answer: Int) : Serializable
--- app/src/main/java/edu/washington/zlin2016/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/zlin2016/quizdroid/MainActivity.kt
@@ -12,67 +12,6 @@ import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
 
-
-    private val physics = Quiz(title="Physics",
-        desc = "Because Physics!",
-        questions = arrayOf(
-            Question(text = "What is fire?",
-                answer = 0,
-                answers= arrayOf(
-                    "One of the four classical elements",
-                    "A magical reaction given to us by God",
-                    "A band that hasn't yet been discovered",
-                    "Fire! Fire! Fire! heh-heh"
-                )
-            )))
-
-    private val math = Quiz(title="Math",
-        desc = "Did you pass the third grade?",
-        questions = arrayOf(
-            Question(text = "What is 2+2?", answer = 0, answers= arrayOf(
-                "4",
-                "22",
-                "An irrational number",
-                "Nobody knows"
-            )
-        )))
-
-    private val marvel = Quiz(title="Marvel Super Heroes",
-        desc = "Avengers, Assemble!",
-        questions = arrayOf(
-            Question(
-                text = "Who is Iron Man?",
-                answer = 0,
-                answers= arrayOf(
-                    "Tony Stark",
-                    "Obadiah Stane",
-                    "A rock hit by Megadeth",
-                    "Nobody knows"
-                )
-            ),
-            Question(
-                text = "Who founded the X-Men?",
-                answer = 1,
-                answers= arrayOf(
-                    "Tony Stark",
-                    "Professor X",
-                    "The X-Institute",
-                    "Erik Lensherr"
-                )
-            ),
-            Question(
-                text = "How did Spider-Man get his powers?",
-                answer = 0,
-                answers= arrayOf(
-                    "He was bitten by a radioactive spider",
-                    "He ate a radioactive spider",
-                    "He is a radioactive spider",
-                    "He looked at a radioactive spider"
-                )
-            )
-        )
-    )
-
     private val quizes = arrayOf(marvel, math, physics)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -89,8 +28,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-
-class Quiz (val title:String, val desc: String, val questions : Array<Question>) : Serializable
-
-class Question(val text: String, val answers: Array<String>, val answer: Int) : Serializable
